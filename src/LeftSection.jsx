0680bceec4f5dae0b3c632f9f8c72720dd2e4045
surajitdutta7995 <surajitdutta7995@gmail.com>
2024-07-17 20:07:57 +0530

    Added logic to implement searcg functionality of watchlist search in left section.

--- src/LeftSection.jsx
+++ src/LeftSection.jsx
@@ -5,20 +5,44 @@ import {
   faRightFromBracket,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { themeRed } from "./ThemeConstants";
 import AppContext from "./AppContext";
 import "./LeftSection.css";
 
 const LeftSection = () => {
-  const { loggedinUser, setLoggedInUser, setIsUserLoggedIn, watchLists, setSelectedWatchlist } =
-    useContext(AppContext);
+  const {
+    loggedinUser,
+    setLoggedInUser,
+    setIsUserLoggedIn,
+    watchLists,
+    setSelectedWatchlist,
+  } = useContext(AppContext);
   const [showLogoutOption, setShowLogoutOption] = useState(false);
+  const [searchParam, setSearchParam] = useState("");
+  const [watchListToRender, setWatchListToRender] = useState([...watchLists]);
+  const updateWatchListToRender = () => {
+    setWatchListToRender(
+      searchParam
+        ? watchLists.filter((list) => list.title.includes(searchParam))
+        : [...watchLists]
+    );
+  };
+  useEffect(() => {
+    updateWatchListToRender();
+  }, [watchLists, searchParam]);
   return (
     <div className="leftSection">
       <div className="listSearch">
         <h1 style={{ color: themeRed }}>Watchlists</h1>
-        <input type="text" placeholder=" Search " className="searchWatchList" />
+        <input
+          type="text"
+          placeholder=" Search "
+          className="searchWatchList"
+          onChange={(e) => {
+            setSearchParam(e.target.value);
+          }}
+        />
         <button className="home">
           <FontAwesomeIcon icon={faHome} style={{ color: "#fff" }} />
           Home
@@ -26,9 +50,17 @@ const LeftSection = () => {
       </div>
       <div className="myListsContainer">
         <h4>My Lists</h4>
-        {watchLists.length > 0 &&
-          watchLists.map((list, index) => (
-            <button className="watchList" key={index} onClick={()=>{setSelectedWatchlist(list)}}>{list.title}</button>
+        {watchListToRender.length > 0 &&
+          watchListToRender.map((list, index) => (
+            <button
+              className="watchList"
+              key={index}
+              onClick={() => {
+                setSelectedWatchlist(list);
+              }}
+            >
+              {list.title}
+            </button>
           ))}
       </div>
       <div className="userSection">
